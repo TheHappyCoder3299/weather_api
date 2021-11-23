eed3178e2b96c68b851fe524b1d0d27a83978747
Anwit Mondal <anwitmondal3299@gmail.com>
2021-11-24 01:43:12 +0530

    html page done, little CSS done

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 axios is a dependency that will call the APIs that we need it is very similar to fetch()
 
 dotenv is a dev dependency that will help us to load the api key
+
+We have added .env and node_modules to .gitignore because we do
+not want to check them into version control , we do not want to expose our API key anywhere 
---
+++ server.js
@@ -0,0 +1,27 @@
+//We make sure that we are not in a production environment
+if(process.env.NODE_ENV !== `production`){
+    //This loads everything in our dotenv file and populates our process.env.ACCUWEATHER_API_KEY
+    require(`dotenv`).config();
+}
+
+
+const ACCUWEATHER_API_KEY=process.env.ACCUWEATHER_API_KEY;
+const express=require(`express`);
+
+const app=express();
+
+app.use(express.json());
+app.use(express.static(`public`));
+
+
+//Our API has only a single endpoint
+
+app.post('/weather',(req,res)=>{
+
+});
+
+app.listen(3000,()=>{
+    //This callback runs when the server has started
+    console.log('Server started');
+})
+
